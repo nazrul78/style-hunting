984da736a7844d12a7dac389004de22919385210
Nazrul Islam <nnakib78@gmail.com>
2023-03-16 01:30:48 +0600

    Video no 38 completed

--- pages/stickers.js
+++ pages/stickers.js
@@ -1,7 +1,202 @@
 import React from "react"
+import Link from "next/link"
 const Stikers = () => {
     return (
-        <div>This is Stikers page</div>
+        <div>
+
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-wrap -m-4">
+                        <Link legacyBehavior href={'/products/wear-the-code'}>
+                            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                                <a className="block relative rounded overflow-hidden">
+                                    <img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/I/419Rsf-mxIL._AC_UL320_.jpg" />
+                                </a>
+                                <div className="mt-4 text-center md:text-left">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Wear The Code</h2>
+                                    <p className="mt-1">$14</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </div>
+                        </Link>
+
+                        <Link legacyBehavior href={'/products/wear-the-code'}>
+                            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                                <a className="block relative rounded overflow-hidden">
+                                    <img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/I/419Rsf-mxIL._AC_UL320_.jpg" />
+                                </a>
+                                <div className="mt-4 text-center md:text-left">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Wear The Code</h2>
+                                    <p className="mt-1">$14</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </div>
+                        </Link>
+
+                        <Link legacyBehavior href={'/products/wear-the-code'}>
+                            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                                <a className="block relative rounded overflow-hidden">
+                                    <img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/I/419Rsf-mxIL._AC_UL320_.jpg" />
+                                </a>
+                                <div className="mt-4 text-center md:text-left">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Wear The Code</h2>
+                                    <p className="mt-1">$14</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </div>
+                        </Link>
+
+                        <Link legacyBehavior href={'/products/wear-the-code'}>
+                            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                                <a className="block relative rounded overflow-hidden">
+                                    <img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/I/419Rsf-mxIL._AC_UL320_.jpg" />
+                                </a>
+                                <div className="mt-4 text-center md:text-left">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Wear The Code</h2>
+                                    <p className="mt-1">$14</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </div>
+                        </Link>
+
+                        <Link legacyBehavior href={'/products/wear-the-code'}>
+                            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                                <a className="block relative rounded overflow-hidden">
+                                    <img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/I/419Rsf-mxIL._AC_UL320_.jpg" />
+                                </a>
+                                <div className="mt-4 text-center md:text-left">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Wear The Code</h2>
+                                    <p className="mt-1">$14</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </div>
+                        </Link>
+
+                        <Link legacyBehavior href={'/products/wear-the-code'}>
+                            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                                <a className="block relative rounded overflow-hidden">
+                                    <img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/I/419Rsf-mxIL._AC_UL320_.jpg" />
+                                </a>
+                                <div className="mt-4 text-center md:text-left">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Wear The Code</h2>
+                                    <p className="mt-1">$14</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </div>
+                        </Link>
+
+                        <Link legacyBehavior href={'/products/wear-the-code'}>
+                            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                                <a className="block relative rounded overflow-hidden">
+                                    <img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/I/419Rsf-mxIL._AC_UL320_.jpg" />
+                                </a>
+                                <div className="mt-4 text-center md:text-left">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Wear The Code</h2>
+                                    <p className="mt-1">$14</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </div>
+                        </Link>
+
+                        <Link legacyBehavior href={'/products/wear-the-code'}>
+                            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                                <a className="block relative rounded overflow-hidden">
+                                    <img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/I/419Rsf-mxIL._AC_UL320_.jpg" />
+                                </a>
+                                <div className="mt-4 text-center md:text-left">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Wear The Code</h2>
+                                    <p className="mt-1">$14</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </div>
+                        </Link>
+
+                        <Link legacyBehavior href={'/products/wear-the-code'}>
+                            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                                <a className="block relative rounded overflow-hidden">
+                                    <img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/I/419Rsf-mxIL._AC_UL320_.jpg" />
+                                </a>
+                                <div className="mt-4 text-center md:text-left">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Wear The Code</h2>
+                                    <p className="mt-1">$14</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </div>
+                        </Link>
+
+                        <Link legacyBehavior href={'/products/wear-the-code'}>
+                            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                                <a className="block relative rounded overflow-hidden">
+                                    <img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/I/419Rsf-mxIL._AC_UL320_.jpg" />
+                                </a>
+                                <div className="mt-4 text-center md:text-left">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Wear The Code</h2>
+                                    <p className="mt-1">$14</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </div>
+                        </Link>
+
+                        <Link legacyBehavior href={'/products/wear-the-code'}>
+                            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                                <a className="block relative rounded overflow-hidden">
+                                    <img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/I/419Rsf-mxIL._AC_UL320_.jpg" />
+                                </a>
+                                <div className="mt-4 text-center md:text-left">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Wear The Code</h2>
+                                    <p className="mt-1">$14</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </div>
+                        </Link>
+
+                        <Link legacyBehavior href={'/products/wear-the-code'}>
+                            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                                <a className="block relative rounded overflow-hidden">
+                                    <img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/I/419Rsf-mxIL._AC_UL320_.jpg" />
+                                </a>
+                                <div className="mt-4 text-center md:text-left">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Wear The Code</h2>
+                                    <p className="mt-1">$14</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </div>
+                        </Link>
+
+                        <Link legacyBehavior href={'/products/wear-the-code'}>
+                            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+                                <a className="block relative rounded overflow-hidden">
+                                    <img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/I/419Rsf-mxIL._AC_UL320_.jpg" />
+                                </a>
+                                <div className="mt-4 text-center md:text-left">
+                                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                                    <h2 className="text-gray-900 title-font text-lg font-medium">Wear The Code</h2>
+                                    <p className="mt-1">$14</p>
+                                    <p className="mt-1">S, M, L, XL, XXL</p>
+                                </div>
+                            </div>
+                        </Link>
+
+
+
+
+
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 
 }
--- pages/tshirts.js
+++ pages/tshirts.js
@@ -7,8 +7,6 @@ const Tshirts = () => {
             <section className="text-gray-600 body-font">
                 <div className="container px-5 py-24 mx-auto">
                     <div className="flex flex-wrap -m-4">
-
-
                         <Link legacyBehavior href={'/products/wear-the-code'}>
                             <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
                                 <a className="block relative rounded overflow-hidden">
