3b9f41859f8cd219860a5a04a33b41f85cb9d1ff
Nazrul Islam <nnakib78@gmail.com>
2023-03-19 17:44:37 +0600

    Resolved some issues

--- pages/mugs.js
+++ pages/mugs.js
@@ -3,7 +3,6 @@ import Link from "next/link"
 const Mugs = () => {
     return (
         <div>
-
             <section className="text-gray-600 body-font">
                 <div className="container px-5 py-24 mx-auto">
                     <div className="flex flex-wrap -m-4">
