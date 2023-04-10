1f9eefd62a72a29de6e354e07165297ff73af95d
Nazrul Islam <nnakib78@gmail.com>
2023-04-10 23:54:15 +0600

    43 no video 7min

--- pages/checkout.js
+++ pages/checkout.js
@@ -2,7 +2,9 @@ import React from "react"
 const Checkout = () => {
     return (
         <div className="container m-auto">
-            <h1>Checkout</h1>
+            <h1 className="font-bold text-3xl my-8 text-center">Checkout</h1>
+            <h2>1. Delivery Details</h2>
+            <div className="mx-auto"></div>
 
         </div>
     )
